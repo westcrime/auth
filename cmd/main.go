1b8a4e1ba7de1833269a385373b04a35ab5a1924
westcrime <westcrime77@rambler.ru>
2025-09-09 19:53:39 +0300

    fix module naming

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
-	desc "auth/pkg/user_v1"
+	desc "github.com/westcrime/auth/pkg/user_v1"
 )
 
 const grpcPort = 50051
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module auth
+module github.com/westcrime/auth
 
 go 1.24.6
 
